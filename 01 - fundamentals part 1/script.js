6e264b0544f723ef4fbd4628b157b067feb07920
Mohamed Ramadan <mr.uiux.dev@gmail.com>
2022-09-02 15:33:53 +0200

    Operators - if/else - ternary operator - switch..etc part 1

--- 01 - fundamentals part 1/script.js
+++ 01 - fundamentals part 1/script.js
@@ -42,7 +42,7 @@ console.log(typeof null); // Object => However this bug is never corrected for l
 /*
     Math       => + - * / % ** 
     Assignment => += -= ++ -- ..etc
-    Comparison => > < >= <= 
+    Comparison => > < >= <= == != === !== 
 */
 let x = 10 + 15; // 25
 x += 5; // 30
@@ -51,10 +51,10 @@ x--; // 30
 console.log(x);
 
 // Challenge #1
-let johnHeight = 1.95;
-let johnMass = 92;
-let markHeight = 1.69;
-let markMass = 78;
+const johnHeight = 1.95;
+const johnMass = 92;
+const markHeight = 1.69;
+const markMass = 78;
 
 const johnBMI = johnMass / johnHeight ** 2;
 const markBMI = markMass / markHeight ** 2;
@@ -62,3 +62,123 @@ console.log(`John BMI: ${johnBMI} & Mark BMI: ${markBMI}`);
 
 const markHigherBMI = markBMI > johnBMI;
 console.log(markHigherBMI);
+
+// Template literals
+const birthYear = 1993;
+const myInfo = `I'm an ${currentJob} and before an ${prevJob}. My current old is ${new Date().getFullYear()} - ${birthYear}
+`;
+console.log(myInfo);
+
+// if / else statements
+let century;
+if (birthYear <= 2000) {
+   century = 20;
+} else {
+   century = 21;
+}
+console.log(century);
+
+// Challenge #2
+if (johnBMI > markBMI) {
+   console.log(
+      `John BMI: ${Math.floor(johnBMI)} higher than Mark BMI: ${Math.floor(
+         markBMI
+      )}`
+   );
+} else {
+   console.log(
+      `Mark BMI: ${Math.floor(markBMI)} higher than John BMI: ${Math.floor(
+         johnBMI
+      )}`
+   );
+}
+
+// Type conversion and coercion
+/*
+    Conversion
+        => Number, String
+        ex: console.log(Number('123'))
+
+    Coercion
+        => Js automatically convert type
+        ex: console.log('20' - 15) // 5 -> number
+            console.log('123' + 2) // 1232 -> string
+*/
+
+// Truthy & Falsely Values
+/*
+    5 false values => null, undefined, NaN, '', 0 
+
+    Js using type coercion of Boolean only in the two case (Logical operator & Logical context)
+*/
+
+// Equality Operators: == vs. ===
+/*
+    == !=     => compare by value only (loose)
+    === !==   => compare by value and type (strict) always use it
+*/
+
+// Boolean Logic & Logical Operators
+/*
+    => || true if one is true
+    => && true if all true
+    => !  inverts true/false
+*/
+
+// Challenge #3
+const dolphinAverageScore = Math.floor((97 + 112 + 80) / 3);
+const koalaAverageScore = Math.floor((109 + 95 + 50) / 3);
+const minScore = 100;
+
+console.log(
+   `Dolphins team scores: ${dolphinAverageScore} and Koalas team scores: ${koalaAverageScore}`
+);
+
+if (
+   dolphinAverageScore > koalaAverageScore &&
+   dolphinAverageScore >= minScore
+) {
+   console.log(`Dolphins team scores: ${dolphinAverageScore} 🏆`);
+} else if (
+   koalaAverageScore > dolphinAverageScore &&
+   koalaAverageScore >= minScore
+) {
+   console.log(`Koalas team scores: ${koalaAverageScore} 🏆`);
+} else if (
+   koalaAverageScore === dolphinAverageScore &&
+   (koalaAverageScore && dolphinAverageScore) >= minScore
+) {
+   console.log("Draw!! Both teams score same average");
+} else {
+   console.log("No team win!!");
+}
+
+// The switch statement
+const day = "Thursday";
+switch (day) {
+   case "Sunday":
+      console.log("Go to gym");
+      break;
+   case "Monday":
+   case "Tuesday":
+   case "Wednesday":
+      console.log("Coding");
+   case "Thursday":
+   case "Friday":
+      console.log("Go to company to work");
+      break;
+   default:
+      console.log("Invalid day");
+      break;
+}
+
+// Ternary Operator
+const ternaryOp = birthYear >= 1993 ? "Yes" : "No";
+console.log(ternaryOp);
+
+// Challenge #4
+const bill = 430;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+console.log(
+   `The bill was ${bill}, the tip was ${tip} and the total ${tip + bill}`
+);
