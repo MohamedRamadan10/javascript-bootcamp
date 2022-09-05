24ab88d73ac098dc5e26de88e93f84a94cae8575
MohamedRamadan10 <mr.uiux.dev@gmail.com>
2022-09-05 16:47:14 +0200

    Arrays

--- 02 - fundamentals part 2/script.js
+++ 02 - fundamentals part 2/script.js
@@ -73,3 +73,62 @@ checkWinner(dolphinScore, koalaScore);
 dolphinScore = calcAvg(85, 54, 41);
 koalaScore = calcAvg(23, 34, 27);
 checkWinner(dolphinScore, koalaScore);
+
+// Array
+const friends = ["Mohamed", "Ahmed", "Brahmi", "Reata"];
+// Another assign
+// const friends = new Array("Mohamed", "Ahmed", "Brahmi", "Reata");
+console.log(friends);
+console.log(friends[friends.length - 1]);
+
+friends[2] = "Mastaba";
+console.log(friends);
+
+const calcBirthAge = (years) => new Date().getFullYear() - years;
+const years = [1993, 2000, 2005, 2010];
+
+const ages = [
+   calcBirthAge(years[0]),
+   calcBirthAge(years[2]),
+   calcBirthAge(years[years.length - 1]),
+];
+console.log(ages);
+
+// Basic Array Methods
+const newLength = friends.push("Racy");
+console.log(friends);
+console.log(newLength);
+
+friends.unshift("John"); // Add element to the begin of array
+console.log(friends);
+
+const popped = friends.pop(); // Remove the last element
+console.log(popped);
+console.log(friends);
+
+friends.shift(); // Remove the first element
+console.log(friends);
+
+console.log(friends.indexOf("ss")); // return index number and if not found return -1
+
+console.log(friends.includes("Ahmed")); // return true / false and using Equality strict
+
+// Challenge #2
+const calcTips = (bill) =>
+   bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+const bills = [125, 555, 444];
+const tips = [calcTips(bills[0]), calcTips(bills[1]), calcTips(bills[2])];
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills, tips, total);
+
+// Objects
+const myInfo = {
+   name: "Mohamed Ramadan",
+   age: new Date().getFullYear() - 1993,
+   job: "Front End Developer",
+   friends: ["Ahmed", "Rama", "Sosa"],
+};
+// Dot vs. Bracket Notation
+
