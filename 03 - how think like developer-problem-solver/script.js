9051b3b98c4ac0af1ad80c3da6307392b0dcf6ba
MohamedRamadan10 <mr.uiux.dev@gmail.com>
2022-09-11 10:36:45 +0200

    Challenge problem solve

--- 03 - how think like developer-problem-solver/script.js
+++ 03 - how think like developer-problem-solver/script.js
@@ -71,3 +71,27 @@ const amplitudeNew = calcAmplitudeNew(
 console.log(amplitudeNew);
 
 // Challenge
+/*
+    (1) Understanding the problem
+        - Array transformed to string, separated by ...
+        - Wha is the X days? index + 1
+
+    (2) Breaking up into sub-problems
+        - Transform array into string
+        - Transform each element to string with C
+        - String needs to contain day (index + 1)
+        - Add ... between element and start end of String
+        - Log string to console
+*/
+const dataOne = [17, 21, 23];
+const dataTwo = [12, 5, -5, 0, 4];
+
+const printForecast = function (arr) {
+   let str = "";
+   for (let i = 0; i < arr.length; i++) {
+      str += `${arr[i]} C in ${i + 1} days...`;
+   }
+   console.log("..." + str);
+};
+
+printForecast(dataOne);
